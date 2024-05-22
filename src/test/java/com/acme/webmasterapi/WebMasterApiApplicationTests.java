b57c4bd265068e0178a6562616b76cbfe4c224f7
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-05-22 14:11:27 -0500

    chore: initial project configuration

---
+++ src/test/java/com/acme/webmasterapi/WebMasterApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acme.webmasterapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebMasterApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
