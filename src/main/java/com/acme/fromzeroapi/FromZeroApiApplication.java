515cb7261c61a0ac06bd06c6efc1d007ff420baa
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-05-29 19:45:27 -0500

    chore: adding Cors configuration

--- src/main/java/com/acme/fromzeroapi/FromZeroApiApplication.java
+++ src/main/java/com/acme/fromzeroapi/FromZeroApiApplication.java
@@ -2,12 +2,26 @@ package com.acme.fromzeroapi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class FromZeroApiApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FromZeroApiApplication.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedOrigins("http://localhost:4200")
+						.allowedMethods("*").allowedHeaders("*");
+			}
+		};
 	}
 
 }
