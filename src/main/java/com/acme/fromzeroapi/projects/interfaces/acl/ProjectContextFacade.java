65460cd8bc49f0fde2069db855a8fb3bd7f6fb03
JorgeQuilla <u20211b197@upc.edu.pe>
2024-06-01 12:24:09 -0500

    feat: project context facade added

--- src/main/java/com/acme/fromzeroapi/projects/interfaces/acl/ProjectContextFacade.java
+++ src/main/java/com/acme/fromzeroapi/projects/interfaces/acl/ProjectContextFacade.java
@@ -1,7 +1,28 @@
 package com.acme.fromzeroapi.projects.interfaces.acl;
 
+import com.acme.fromzeroapi.projects.domain.model.aggregates.Project;
+import com.acme.fromzeroapi.projects.domain.model.queries.GetAllProjectsQuery;
+import com.acme.fromzeroapi.projects.domain.services.ProjectQueryService;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 public class ProjectContextFacade {
+    private final ProjectQueryService projectQueryService;
+
+    public ProjectContextFacade(ProjectQueryService projectQueryService) {
+        this.projectQueryService = projectQueryService;
+    }
+
+    /*
+     * Se espera que el contexto que utilize este metodo
+     * haga un resource el cual manipule o haga uso de
+     * algunos atributos de la entidad Project que se
+     * está devolviendo
+     * */
+    public List<Project> getAllProjects(){
+        var getAllProjectsQuery = new GetAllProjectsQuery();
+        return this.projectQueryService.handle(getAllProjectsQuery);
+    }
 }
