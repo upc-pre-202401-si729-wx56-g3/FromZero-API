f73b981fcbcfc915a6ec2f5bdef6a3c0e9407145
jufer10 <fcarrasco023@gmail.com>
2024-06-06 21:31:43 -0500

    feat: create enterprise

--- src/main/java/com/acme/fromzeroapi/home/domain/model/commands/CreateEmpresarioCommand.java
+++
@@ -1,4 +0,0 @@
-package com.acme.fromzeroapi.home.domain.model.commands;
-
-public record CreateEmpresarioCommand(String country, int businessName, int cellphone, String email, String website, String section) {
-}
