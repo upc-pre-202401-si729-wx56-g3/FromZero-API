007ed08aa060dc0540a8dd802f30220d785e8898
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-06-06 22:48:34 -0500

    feat: refactoring name for controller

--- src/main/java/com/acme/fromzeroapi/auth/interfaces/rest/AuthController.java
+++ src/main/java/com/acme/fromzeroapi/auth/interfaces/rest/AuthController.java
@@ -1,14 +1,5 @@
 package com.acme.fromzeroapi.auth.interfaces.rest;
 
-import com.acme.fromzeroapi.auth.domain.model.aggregates.User;
-import com.acme.fromzeroapi.auth.domain.model.commands.CreateUserCommand;
-import com.acme.fromzeroapi.auth.domain.model.commands.SignUpDeveloperCommand;
-import java.util.Optional;
-
-import com.acme.fromzeroapi.auth.domain.model.commands.SignUpEnterpriseCommand;
-import com.acme.fromzeroapi.auth.domain.model.queries.GetAllUsersQuery;
-import com.acme.fromzeroapi.auth.domain.model.queries.GetUserByEmailQuery;
-import com.acme.fromzeroapi.auth.domain.model.queries.GetUserByIdQuery;
 import com.acme.fromzeroapi.auth.domain.services.UserCommandService;
 import com.acme.fromzeroapi.auth.domain.services.UserQueryService;
 import com.acme.fromzeroapi.auth.interfaces.rest.resources.*;
@@ -21,18 +12,15 @@ import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.server.ResponseStatusException;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/v1/api/auth")
-@Tag(name = "Users", description = "Operations related to users")
-public class UserController {
+@Tag(name = "Auth", description = "Operations related to users")
+public class AuthController {
     private final UserCommandService userCommandService;
 //  private final UserQueryService userQueryService;
 
-    public UserController(UserCommandService userCommandService, UserQueryService userQueryService) {
+    public AuthController(UserCommandService userCommandService, UserQueryService userQueryService) {
         this.userCommandService = userCommandService;
         //this.userQueryService = userQueryService;
     }
