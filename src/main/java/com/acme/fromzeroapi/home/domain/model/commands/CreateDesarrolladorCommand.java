2656cd4356d88153d10ebdfd83273ecc38b4305b
jufer10 <fcarrasco023@gmail.com>
2024-06-06 21:30:47 -0500

    feat: create desarrollador

--- src/main/java/com/acme/fromzeroapi/home/domain/model/commands/CreateDesarrolladorCommand.java
+++
@@ -1,4 +0,0 @@
-package com.acme.fromzeroapi.home.domain.model.commands;
-
-public record CreateDesarrolladorCommand(String country, int cellphone, String email, int projectQuantity, String specialities) {
-}
--- src/main/java/com/acme/fromzeroapi/home/interfaces/rest/resources/CreateDesarrolladorResource.java
+++
@@ -1,4 +0,0 @@
-package com.acme.fromzeroapi.home.interfaces.rest.resources;
-
-public record CreateDesarrolladorResource(String country, int cellphone, String email, int projectQuantity, String specialities) {
-}
