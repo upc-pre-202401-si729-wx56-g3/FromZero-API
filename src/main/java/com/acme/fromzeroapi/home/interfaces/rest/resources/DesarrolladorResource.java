e54129d172f77dac65b7a13f61ef2211e352abeb
jufer10 <fcarrasco023@gmail.com>
2024-06-06 21:33:41 -0500

    feat: desarrollador

--- src/main/java/com/acme/fromzeroapi/home/interfaces/rest/resources/DesarrolladorResource.java
+++
@@ -1,4 +0,0 @@
-package com.acme.fromzeroapi.home.interfaces.rest.resources;
-
-public record DesarrolladorResource(Long id, String country, int cellphone, String email, int projectQuantity, String specialities) {
-}
