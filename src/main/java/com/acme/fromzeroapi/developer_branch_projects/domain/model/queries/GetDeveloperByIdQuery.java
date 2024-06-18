382576a0ba4ae4e326bcca44f9c64e57ea77386a
Purak189 <106536136+Purak189@users.noreply.github.com>
2024-06-18 15:13:52 -0500

    fix: deleting developer and enterprise facade test

--- src/main/java/com/acme/fromzeroapi/developer_branch_projects/domain/model/queries/GetDeveloperByIdQuery.java
+++
@@ -1,4 +0,0 @@
-package com.acme.fromzeroapi.developer_branch_projects.domain.model.queries;
-
-public record GetDeveloperByIdQuery(Long id) {
-}
--- src/main/java/com/acme/fromzeroapi/enterprise_branch_projects/domain/model/queries/GetEnterpriseByIdQuery.java
+++
@@ -1,4 +0,0 @@
-package com.acme.fromzeroapi.enterprise_branch_projects.domain.model.queries;
-
-public record GetEnterpriseByIdQuery(Long enterpriseId) {
-}
